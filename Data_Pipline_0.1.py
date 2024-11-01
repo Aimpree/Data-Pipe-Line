6e55e50dafe9300924f3cd05e48beba6d6fb6db8
Aimpree <Aimpree.sikkamonton@gmail.com>
2024-11-02 00:52:20 +0700

    # Add Denoisse Images

--- Data_Pipline_0.1.py
+++ Data_Pipline_0.1.py
@@ -10,7 +10,7 @@ def RGB2BINARY_Transform(Input: str, Output_Folder1: str, Output_Folder2: str,
     
     Gray_Image = cv2.imread(Input, cv2.IMREAD_GRAYSCALE)
 
-    _, Tran_Image = cv2.threshold(Gray_Image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    _, Tran_Image = cv2.threshold(Gray_Image, 48, 255, cv2.THRESH_BINARY) ## + cv2.THRESH_OTSU
     
     Output_Path1 = os.path.join(Output_Folder1, Save_As_Name1)
     Output_Path2 = os.path.join(Output_Folder2, Save_As_Name2)
@@ -93,24 +93,67 @@ def Create_TXT(Input_List_Of_Data: list, Number_Of_Chunk:
             file.write(' '.join(map(str, row)) + '\n')
 
 
+def De_Noisse(Input: str, Output_Folder: str, Save_As_Name: str, H: int = 70):
+
+    os.makedirs(Output_Folder, exist_ok=True)
+
+    image_bw = cv2.imread(Input, cv2.IMREAD_GRAYSCALE)
+    
+    noiseless_image_bw = cv2.fastNlMeansDenoising(image_bw, None, H, templateWindowSize=7, searchWindowSize=21)
+
+    Output_Path = os.path.join(Output_Folder, Save_As_Name)
+
+    cv2.imwrite(Output_Path, noiseless_image_bw)
+
+def something2():
+    pass
+
+
 def main(Name: str, Input: str, Class_Number: int, Number_of_Chunk: int):
+
+    Starttime = time.time()
+    
 
     Final_Output = f'Process Images {Name}'
     os.makedirs(Final_Output, exist_ok=True)
 
     Count_Origin_Images = os.listdir(Input)
+
+    New_file_Type = []
+
     Output_Binary_Images = f'{Final_Output}/Binary Images'
+
     Output_Gray_Images = f'{Final_Output}/Gray Images'
+
+    Output_De_Noiss_Images = f'{Final_Output}/Denoiss Images'
+
     Output_Label_File = f'{Final_Output}/Label Text File'
 
+    for i in Count_Origin_Images:
+
+        Change_Type = i.replace('.jpg', '.txt')
+        New_file_Type.append(Change_Type)
+    
+    print(f'Starting Process RGB to BINARY')
+
     for i in range(len(Count_Origin_Images)):
-        
+
         RGB2BINARY_Transform(f'{Input}/{Count_Origin_Images[i]}', Output_Binary_Images, 
                              Output_Gray_Images, f'BR_{Count_Origin_Images[i]}', f'GR_{Count_Origin_Images[i]}')
+        
+        print(f'Save.... BR_{Count_Origin_Images[i]}')
+
+    print(f'Starting Process Denoisse')
     
-    time.sleep(2)
+    for i in range(len(Count_Origin_Images)):
+
+        De_Noisse(f'./Process Images {Name}//Binary Images/BR_{Count_Origin_Images[i]}', Output_De_Noiss_Images, f'BR_DE_{Count_Origin_Images[i]}')
+
+        print(f'Save.... BR_DE_{Count_Origin_Images[i]}')
 
     Count_Binary_Images = os.listdir(Output_Binary_Images)
+
+    print(f'Starting Process Create txt file')
 
     for i in range(len(Count_Binary_Images)):
 
@@ -119,17 +162,22 @@ def main(Name: str, Input: str, Class_Number: int, Number_of_Chunk: int):
                                      f'DP_{Count_Binary_Images[i]}', Class_Number)
         
         Create_TXT(List_of_Data, Number_of_Chunk, 
-                   Count_Origin_Images[i], Output_Label_File)
-
+                   New_file_Type[i], Output_Label_File)
+        
+        print(f'Save.... {New_file_Type[i]}')
     
-    print("Compleat Created")
+    end = time.time()
+    print("Compleat All")
+    print(f'{end-Starttime}: sec')
+
+
     
 
 if __name__ == "__main__":
 
-    Name = "RD41"
+    Name = "test2"
     Input_Path = 'C:/Users/Aimpr/OneDrive/Desktop/Rice dataset2/Original Picture/RD41'
     Number_of_Chunk = 5
-    Class_Number = 0
+    Class_Number = 2
 
     main(Name, Input_Path, Class_Number, Number_of_Chunk)
